819c0dd35ff500fc13c9f37234c02db2497fd46d
dwanderton <dwanderton@gmail.com>
2013-09-18 20:41:30 +0000

    Added functions.js as a central repository for commonly used functions and enable us to tidy existing code

---
+++ functions.js
@@ -0,0 +1,16 @@
+/*
+
+This is the NewsGrid Central Function Repository
+
+*/
+var NewsGridFunction = {
+
+/* examples of how to pupulate:  
+   TWITTER_TWEET: "This student crowdfunder looks interesting.",
+  days_left: function() {
+      return Math.max(Math.ceil((this.FUNDING_END_DATE - new Date()) / (1000*60*60*24)), 0);
+  }
+*/
+};
+
+module.exports = NewsGridFunction;
